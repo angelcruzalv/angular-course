59b14c81f0cb896e3fb751c7abc77c95635be101
Angel Cruz <37281529+angelcruzalv@users.noreply.github.com>
2021-07-14 16:15:19 -0500

    03 TS Excercise
    03-TS-FUNCTIONS

---
+++ intro-typescript-ecmascript-1.0.1/src/exercises/03-functions.ts
@@ -0,0 +1,54 @@
+
+ 
+
+/*
+    ===== TypeScript code =====
+*/
+//No type specify
+//  function sum (a , b){
+//      return a+b;
+//  }
+
+ function sum(a:number, b:number):number {
+     return a + b;
+ }
+
+ const sumArrow =(a: number, b: number):number => {return a + b}
+
+ 
+ //const mult: number = (a, b) => {return a*b} //error test
+ 
+ const mult = (num:number, num2?:number, num3:number = 8):number => {
+     return num * num3;
+     }
+    
+// const res = mult(5,  4);
+
+// console.log(res);
+
+//Object Character
+interface CharacMine{
+    name: string,
+    hp: number,
+    showHP: () => void; //function as a argument
+}
+//new healing function calls the character
+function healing(character: CharacMine, healX: number): void{
+
+    character.hp += healX; //increments heal
+    
+}
+//create a new character CharacMine's object
+//
+const newCharacter: CharacMine = {
+    name: 'Jose',
+    hp: 45,
+    showHP(){
+        //show the new hp number
+        console.log('Health Points: ' + this.hp );
+    }
+}
+//adds hp to the object newCharacter
+healing(newCharacter, 20);
+//calls the function to show the new HP
+newCharacter.showHP(); 
