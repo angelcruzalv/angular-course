958f903ca8bf49eab049cc683f1e11f96e633241
Angel Cruz <37281529+angelcruzalv@users.noreply.github.com>
2021-07-14 14:09:57 -0500

    01 TS Exercise

---
+++ intro-typescript-ecmascript-1.0.1/src/exercises/01_types.ts
@@ -0,0 +1,19 @@
+
+ 
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let user = 'Michael';
+
+let age: number | string = 30;
+
+let x: string = 'X';
+
+let y: number = 5;
+
+let z= 4;
+
+console.log (user, age)
+
